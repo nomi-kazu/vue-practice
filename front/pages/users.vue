e2f7f13251a4d38d55276991f27a2a9fcff1db5b
nomi-kazu <nomikazyu@gmail.com>
2021-08-22 19:06:24 +0900

    feat: users.vueを作成し、nuxtでusersのデータを取得

---
+++ front/pages/users.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col>
+        <h1>Hello, World!</h1>
+      </v-col>
+    </v-row>
+
+    <v-card
+      class="mx-auto"
+      max-width="300"
+      title
+    >
+      <v-list rounded>
+        <v-subheader>USERS</v-subheader>
+        <v-list-item-group color="primary">
+          <v-list-item
+            v-for="user in users"
+            :key="users.id"
+            @click=""
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="user.name"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import axios from "~/plugins/axios"
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+
+  created() {
+    // userをaxiosで取得
+    axios.get("/users").then(res => {
+      if (res.data) {
+        this.users = res.data
+      }
+    })
+  }
+}
+</script>
