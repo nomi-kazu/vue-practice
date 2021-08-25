08356a518d68161e25572248445dafb1f5bb580d
nomi-kazu <nomikazyu@gmail.com>
2021-08-25 21:31:29 +0900

    feat: plugins/firebaseの作成、axiosの設定修正など

--- front/plugins/axios.js
+++ front/plugins/axios.js
@@ -1,7 +1,14 @@
 export default function({ $axios, redirect }) {
-  $axios.setToken('access_token')
-
-  $axios.onResponse(config => {
-    $axios.setHeader('Access-Control-Allow-Origin', '*')
-  })
+    $axios.setToken('access_token')
+    
+    $axios.onResponse(config => {
+        $axios.setHeader('Access-Control-Allow-Origin', '*')
+    })
+    baseURL: process.env.API_ENDPOINT
 }
+
+// import axios from "axios";
+
+// export default axios.create({
+//   baseURL
+// })
